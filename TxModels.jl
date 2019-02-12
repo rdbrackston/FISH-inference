9fba30896de5518e3ad406dea55a16246f310919
leham <45585190+leham@users.noreply.github.com>
2019-02-12 19:55:56 +1100

    Update TxModels.jl

--- TxModels.jl
+++ TxModels.jl
@@ -258,8 +258,16 @@ end
 """
 Function to evaluate the analytical distribution for the leaky gene expression model.
 Called by solvemaster for cases in which parameters has length four.
-!! CURRENTLY INCORRECT !!
+!! Hopefully OK now !!
 """
+function fracrise(x, y, r)
+Q = 1
+	for m=0:r-1
+	Q *= (x + m) / (y + m)
+	end 
+return Q
+end 
+	
 function solvemaster_full(parameters, N)
 
 	# Assign the parameters
@@ -273,10 +281,7 @@ function solvemaster_full(parameters, N)
     for n=0:N-1
     	for r=0:n
     		# P[n+1] += binomial(big(n),big(r))*K₁^(n-r)*(K₀-K₁)^r*GSL.hypergeom(ν+r,λ+ν+r,K₁-K₀)
-    		P[n+1] += binomial(big(n),big(r))*K₁^(n-r)*exp(-K₁)*(K₀-K₁)^r*GSL.hypergeom(ν+r,λ+ν+r,K₁-K₀)
-    		for m=0:r-1
-	            P[n+1] *= (ν+m)/(ν+λ+m)
-	        end
+    		P[n+1] += binomial(big(n),big(r))*K₁^(n-r)*exp(-K₁)*(K₀-K₁)^r*GSL.hypergeom(ν+r,λ+ν+r,K₁-K₀)*fracrise(ν,ν+λ,r)
     	end
     	P[n+1] = P[n+1]/factorial(big(n))
     end
