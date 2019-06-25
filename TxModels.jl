f4b31a9a35e558f3dceaad96a65d21c1491975e5
Rowan Brackston <rowan.brackston@cantab.net>
2019-06-25 17:54:15 +0100

    New hypergeometric function evaluation

--- TxModels.jl
+++ TxModels.jl
@@ -92,7 +92,7 @@ drawn from a distribution. Recursively marginalises over each parameter in turn.
 function solvecompound(parameters::AbstractArray, hyperParameters::AbstractArray,
                        distFunc::Symbol, parIndex=[1]; lTheta::Integer=200,
                        cdfMax::AbstractFloat=0.999, N::Union{Symbol,Integer}=:auto,
-                       verbose=false)
+                       verbose=false, method=:fast)
     
     # Choose mixing distribution. The mean and variance are identical from case to case.
     m = parameters[parIndex[1]]
@@ -128,9 +128,13 @@ function solvecompound(parameters::AbstractArray, hyperParameters::AbstractArray
     	parMod[parIndex[1]] = thet
     	if length(parIndex) > 1
     		PVec[ii] = solvecompound(parMod, hyperParameters[2:end], distFunc,
-    			parIndex[2:end], lTheta=lTheta, cdfMax=cdfMax, N=N)
+    			parIndex[2:end], lTheta=lTheta, cdfMax=cdfMax, N=N, method=method)
     	else
-        	PVec[ii] = solvemaster(parMod, N, verbose)
+    		if isequal(method,:fast)
+        		PVec[ii] = solvemaster(parMod, N, verbose)
+        	else
+        		PVec[ii] = solvemaster2(parMod, N, verbose)
+        	end
         end
         L = max(L,length(PVec[ii])-1)
     end
@@ -148,10 +152,10 @@ function solvecompound(parameters::AbstractArray, hyperParameters::AbstractArray
     for ii=1:length(Q)
 
         # Perform the integration using the trapezium rule
-        Q[ii] += PVec[1][ii]*pdf(parDistribution,thetVec[1])
-        Q[ii] += PVec[end][ii]*pdf(parDistribution,thetVec[end])
+        Q[ii] += PVec[1][ii]*Distributions.pdf(parDistribution,thetVec[1])
+        Q[ii] += PVec[end][ii]*Distributions.pdf(parDistribution,thetVec[end])
         for jj = 2:length(thetVec)-1
-            Q[ii] += 2*PVec[jj][ii]*pdf(parDistribution,thetVec[jj])
+            Q[ii] += 2*PVec[jj][ii]*Distributions.pdf(parDistribution,thetVec[jj])
         end
 
     end
@@ -284,6 +288,99 @@ function simcompound(parameters::AbstractArray, hyperParameters::AbstractArray,
 	end
 
 	return Integer.(round.(simData))
+
+end
+
+
+"""
+Function to solve the master equation at steady state, returning the probability
+mass function. Evaluation uses the recursive implementation of the hypergeometric
+function. This is slower but more robust than the GSL version.
+"""
+function solvemaster2(parameters, N=:auto::Union{Symbol,Int64}, verbose=false)
+
+    λ = parameters[2]
+    ν = parameters[3]
+    K = parameters[1]
+    δ = 1.0
+    E = λ*K/(λ+ν)/δ    # Mean
+    V = λ*K/(λ+ν)/δ + λ*ν*K^2/((λ+ν)^2)/(λ+ν+δ)/δ    # Variance
+    
+    # Rule of thumb for maximum number of mRNA with non-negligible probability
+    Nguess = Int(round(E+5*sqrt(V)))
+    if N==:auto
+        N = Nguess
+    end
+
+    a = λ/δ
+    b = (λ+ν)/δ
+    c = K/δ
+
+    P = zeros(N)
+    global F = GSL.hypergeom(a,b,-c)
+    P[1] = F
+    for n=1:N-1
+        M = max(5,Int(round(100*n/c)))
+        F = hypfun_rec_miller(a+n-1,b+n-1,-c,1,M,F)
+        P[n+1] = c^n * F/factorial(big(n))
+        # Use Stirling's approximation for n!, splitting power computation into 2
+        if Base.isinf(P[n+1])
+            n1 = floor(n/2)
+            n2 = floor(n/2) + n%2
+            x = c*ℯ/n
+            P[n+1] = F * x^n1 * x^n2 / sqrt(2*n*pi)
+            # Now split power computation into 3
+            if Base.isinf(P[n+1])
+                n1 = floor(n/3)
+                n2 = floor(n/3)
+                n3 = floor(n/3) + n%3
+                x = c*ℯ/n
+                P[n+1] = F * x^n1 * x^n2 * x^n3 / sqrt(2*n*pi)
+            end
+        end
+        for m=0:n-1
+            P[n+1] *= (a+m)/(b+m)
+        end
+    end
+
+    return P
+    
+end
+
+
+"""
+Computes the hypergeometric function 1F1(a+k;b+k;z), for k a large
+positive integer, using M(a;b;z) and Miller's algorithm, with
+n a number much larger than k.
+
+Based on code copyright John W. Pearson 2014
+"""
+function hypfun_rec_miller(a::Float64,b::Float64,z::Float64,k::Int64, n=:auto, sol=:none)
+
+    if n==:auto
+        n = Int(50*k)
+    end
+
+    # Initialise f and v in Miller's algorithm
+    f = big.(zeros(k+1,1))
+    v = big.(zeros(n+1,1))
+
+    # Input minimal solution with k=0
+    if sol==:none
+        f1 = GSL.hypergeom(a,b,z)/SpecialFunctions.gamma(b)
+    else
+        f1 = sol/SpecialFunctions.gamma(big(b))
+    end
+    v[end] = 0
+    v[end-1] = 1
+
+    # Compute recurrence backwards
+    for i2 = 2:n
+        v[n+1-i2] = (v[n+3-i2] + (b+n+1-i2-z-1)*v[n+2-i2]/((a+n+1-i2)*z)) * ((a+n+1-i2)*z)
+    end
+
+    # Return solution as last line of Miller's algorithm
+    return f1/v[1]*v[k+1]*SpecialFunctions.gamma(big(b+k))
 
 end
 
