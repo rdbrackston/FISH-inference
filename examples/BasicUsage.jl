2a64353493eaf8095c416be660c4ca19225ad100
Rowan Brackston <rowan.brackston@cantab.net>
2020-03-24 18:33:55 +0000

    Initial basic usage examples

---
+++ examples/BasicUsage.jl
@@ -0,0 +1,16 @@
+# Basic usage examples for evaluating distributions and sampling
+
+using TxModels,  Plots
+gr()
+
+# Solve the master equation for the Telegraph distribution
+prms = [40,0.6,0.4]
+P = solvemaster(prms,60)
+plot(0:length(P)-1,P, xlabel="Copy no., n",ylabel="Probability, p(n)",
+    label="Analytic solution")
+
+# Sample from the distribution and compare the histogram
+dist = TelegraphDist(prms...)
+samps = rand(dist,800)
+x,y = TxModels.genpdf(samps)
+plot!(x,y, label="Sampled histogram", line=0,marker=3)
